1503454bc538fdfdaaff3710054a1556bfebbfc5
josimar <josimarsofive@example.com>
2021-01-21 17:43:21 -0500

    Managed driver browser chrome

--- src/test/kotlin/ExampleTests.kt
+++ src/test/kotlin/ExampleTests.kt
@@ -1,18 +1,21 @@
+import net.serenitybdd.junit.runners.SerenityRunner
 import net.serenitybdd.screenplay.Actor
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb
 import net.serenitybdd.screenplay.actions.Open
+import net.thucydides.core.annotations.Managed
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 
+@RunWith(SerenityRunner::class)
 class ExampleTests {
 
+    @Managed
     var webDriver: WebDriver? = null
 
     @Test
     fun navigateToContactPage(){
-        System.setProperty("webdriver.chrome.driver","C:\\repo\\browser\\chromedriver.exe")
-        webDriver = ChromeDriver()
 
         val josimar = Actor.named("Josimar")
         josimar.can(BrowseTheWeb.with(webDriver))
